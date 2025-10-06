146e3838046971c301cb89a4c39b0c3ad4ebf68e
Mihail-Hristov-123 <mihail.hristov@intellias.com>
2025-10-06 17:20:12 +0300

    feat: create weather API service

--- weather-app/src/store.ts
+++ weather-app/src/store.ts
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { weatherApi } from "./services/weatherApi";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [weatherApi.reducerPath]: weatherApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(weatherApi.middleware),
 });
-
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;

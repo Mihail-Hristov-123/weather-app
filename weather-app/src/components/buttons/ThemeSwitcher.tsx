0cd5640f98a644abbde44f853b29b457874dfd2a
Mihail-Hristov-123 <mihail.hristov@intellias.com>
2025-10-08 11:34:28 +0300

    refactor: place all SVGs into the assets directory

--- weather-app/src/components/buttons/ThemeSwitcher.tsx
+++ weather-app/src/components/buttons/ThemeSwitcher.tsx
@@ -1,16 +1,16 @@
 import { useTheme } from "../../contexts/ThemeContext";
-import { DarkModeSVG } from "../SVG/DarkModeSVG";
-import { LightModeSVG } from "../SVG/LightModeSVG";
+import darkModeSVG from "../../assets/dark-mode.svg";
+import lightModeSVG from "../../assets/light-mode.svg";
 
 export const ThemeSwitcher = () => {
   const { toggleDarkMode, darkMode } = useTheme();
 
   return (
     <button
-      className="fixed size-20 z-10 bottom-8 dark:bg-white   right-0 p-2 pr-6 border-2 border-blue-950 border-r-0 rounded-l-2xl"
+      className={`fixed w-24 h-18 z-10 bottom-8 bg-white  cursor-pointer right-0 px-2  border-2 border-blue-950 border-r-0 rounded-l-2xl `}
       onClick={toggleDarkMode}
     >
-      {darkMode ? <LightModeSVG /> : <DarkModeSVG />}
+      <img src={darkMode ? darkModeSVG : lightModeSVG} alt="" />
     </button>
   );
 };
